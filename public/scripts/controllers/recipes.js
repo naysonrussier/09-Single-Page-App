2227633fd1c9730961b6c4fb32278f72d8b57039
Nayson Russier <nayson.russier@student-clc.fr>
2017-03-19 09:51:49 +0100

    cleaning js files

--- public/scripts/controllers/recipes.js
+++ public/scripts/controllers/recipes.js
@@ -1,44 +1,58 @@
-'use strict';
 
-angular.module('app')
-.controller('RecipesController', function($scope, dataService, $location) {
+(function() {
+  'use strict';
 
+  /*
+This file provides the necessary controllers for the the recipe.html page to work
+*/
 
-	$scope.getRecipes = function() {
-		dataService.getRecipes(function(response) {
-		  $scope.recipes = response.data
-		});
-	}
-	
-	$scope.getCategories = function() {
-		dataService.getCategories(function(response) {
-		  $scope.categories = response.data;
-		});
-	}
+	angular.module('app')
+	.controller('RecipesController', function($scope, dataService, $location) {
 
-	$scope.clear = function() {
-		$scope.getRecipes();
-		$scope.getCategories();
-	}
-	
- 	$scope.filterByCategory = function() {
-		dataService.getRecipesByCategory($scope.item.name, function(response) {
-		  $scope.recipes = response.data
-		});
-	}
-	 
-	$scope.deleteData = function(id) {
-		var answer = confirm("Are you sure you want to delete this recipe?")
-		if (answer) {
-			dataService.delRecipeById(id);
-			$location.path('/').replace();
+	//Get data
+		//get a list of the recipes
+		$scope.getRecipes = function() {
+			dataService.getRecipes(function(response) {
+			  $scope.recipes = response.data
+			});
 		}
+		
+		//get a list of the categories
+		$scope.getCategories = function() {
+			dataService.getCategories(function(response) {
+			  $scope.categories = response.data;
+			});
+		}
+		
+		//refresh the lists 
+		$scope.clear = function() {
+			$scope.getRecipes();
+			$scope.getCategories();
+		}
+		
+		//filter all the recipe by a specific category
+		$scope.filterByCategory = function() {
+			dataService.getRecipesByCategory($scope.item.name, function(response) {
+			  $scope.recipes = response.data
+			});
+		}
+		 
+		//delete a specific recipe, with a confirmation box
+		$scope.deleteData = function(id) {
+			var answer = confirm("Are you sure you want to delete this recipe?")
+			if (answer) {
+				dataService.delRecipeById(id);
+				$location.path('/').replace();
+			}
+			$scope.clear();
+		}
+		
+		//navigate to the recipe-detail.html, to add a new recipe
+		$scope.addNew = function() {
+			$location.path('/add').replace();
+		}
+		
+		//starting the app
 		$scope.clear();
-	}
-  
-	$scope.addNew = function() {
-		$location.path('/add').replace();
-	}
-	
-	$scope.clear();
-});
+	});
+})()
--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -1,54 +1,57 @@
-'use strict';
 
-angular.module('app')
-.service("dataService", function($http) {
-	
-  this.getRecipes = function(success, error) {
-    $http.get('api/recipes')
-    .then(success)
-	.catch(error)
-  };
-  
-  this.getCategories = function(success, error) {
-    $http.get('api/categories')
-    .then(success)
-	.catch(error)
-  };
-  
-  this.getFoodItems = function(success, error) {
-    $http.get('api/fooditems')
-    .then(success)
-	.catch(error)
-  };
-  
-  this.getRecipesByCategory = function(category, success, error) {
-    $http.get('api/recipes?category='+ category)
-    .then(success)
-	.catch(error)
-  }
-  
-  this.getRecipesById = function(id, success, error) {
-    $http.get('api/recipes/'+ id)
-	.then(success)
-	.catch(error)
-  }
-  
-  this.updateRecipesById = function(id, recipe, success, error) {
-    $http.put('api/recipes/'+id, recipe)
-    .then(success)
-	.catch(error)
-  }
-  
-  this.postRecipe = function(recipe, success, error) {
-    $http.post('api/recipes', recipe)
-    .then(success)
-	.catch(error)
-  }
-  
-  this.delRecipeById = function(id, success, error) {
-    $http.delete('api/recipes/'+id)
-    .then(success)
-	.catch(error)
-  }
-  
-});
+(function() {
+  'use strict';
+
+	angular.module('app')
+	.service("dataService", function($http) {
+		
+	  this.getRecipes = function(success, error) {
+		$http.get('api/recipes')
+		.then(success)
+		.catch(error)
+	  };
+	  
+	  this.getCategories = function(success, error) {
+		$http.get('api/categories')
+		.then(success)
+		.catch(error)
+	  };
+	  
+	  this.getFoodItems = function(success, error) {
+		$http.get('api/fooditems')
+		.then(success)
+		.catch(error)
+	  };
+	  
+	  this.getRecipesByCategory = function(category, success, error) {
+		$http.get('api/recipes?category='+ category)
+		.then(success)
+		.catch(error)
+	  }
+	  
+	  this.getRecipesById = function(id, success, error) {
+		$http.get('api/recipes/'+ id)
+		.then(success)
+		.catch(error)
+	  }
+	  
+	  this.updateRecipesById = function(id, recipe, success, error) {
+		$http.put('api/recipes/'+id, recipe)
+		.then(success)
+		.catch(error)
+	  }
+	  
+	  this.postRecipe = function(recipe, success, error) {
+		$http.post('api/recipes', recipe)
+		.then(success)
+		.catch(error)
+	  }
+	  
+	  this.delRecipeById = function(id, success, error) {
+		$http.delete('api/recipes/'+id)
+		.then(success)
+		.catch(error)
+	  }
+	  
+	});
+})();
