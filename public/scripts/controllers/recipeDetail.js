ac34f0f06bf1cd3c3716a63d5fc51484cce46169
Nayson Russier <nayson.russier@student-clc.fr>
2017-03-19 09:56:18 +0100

    cleaning

--- public/scripts/controllers/recipeDetail.js
+++ public/scripts/controllers/recipeDetail.js
@@ -1,4 +1,3 @@
-
 (function() {
   'use strict';
 /*
@@ -32,9 +31,9 @@ This file provides the necessary controllers for the the recipe-detail.html page
 		//This function will let the user know when there are errors
 		function error(message) {
 			if(message == "") {
-				$scope.errors.push({message: "An error as occured. Please check your internet connection, or try again."})
+				$scope.errors.push({message: "An error as occured. Please check your internet connection, or try again."});
 			} else {
-				$scope.errors.push({message : message})
+				$scope.errors.push({message : message});
 			}
 		}
 		
@@ -43,8 +42,8 @@ This file provides the necessary controllers for the the recipe-detail.html page
 			if(message != null) {
 				alert(message);
 			}
-			$location.path("/")
-		}
+			$location.path("/");
+		};
 		
 		//This function catch the correct recipe, base on his ID
 		//If ths user is just adding a new recipe, it will only provide the structure of a recipe
@@ -53,18 +52,18 @@ This file provides the necessary controllers for the the recipe-detail.html page
 				dataService.getRecipesById($scope.recipeId, function(res) {
 					$scope.recipe = res.data;
 				}, function(res) {
-					error(res.data)
+					error(res.data);
 				});
 			} else {
 				$scope.recipe = recipeDetails();
 			}
-		}
+		};
 		
 		//This function will save or add a new recipe
 		$scope.saveRecipe = function() {
 			function success() {
 				$scope.edited = false;
-				$scope.returnHome("Your recipe has been successfully saved!!!")
+				$scope.returnHome("Your recipe has been successfully saved!!!");
 			}
 			if($scope.recipeId != "") {
 				dataService.updateRecipesById($scope.recipeId,$scope.recipe,success(),function(res) {
@@ -72,7 +71,7 @@ This file provides the necessary controllers for the the recipe-detail.html page
 				});
 			} else {
 				dataService.postRecipe($scope.recipe,success(),function(res) {
-					error(res.statusText)
+					error(res.statusText);
 				});
 			}
 		};
@@ -83,16 +82,16 @@ This file provides the necessary controllers for the the recipe-detail.html page
 		};
 		
 		$scope.deleteIngredient = function(step, $index) {
-			$scope.recipe.ingredients.splice($index, 1)
-		}
+			$scope.recipe.ingredients.splice($index, 1);
+		};
 			
 		$scope.addSteps = function() {
 			$scope.recipe.steps.push(recipeDetails_steps());
 		};
 		
 		$scope.deleteStep = function($index) {
-			$scope.recipe.steps.splice($index, 1)
-		}
+			$scope.recipe.steps.splice($index, 1);
+		};
 		
 	//Get data
 		//get the recipe details
@@ -117,7 +116,7 @@ This file provides the necessary controllers for the the recipe-detail.html page
 		//if edited is true, the pop up an alert box when leaving the page
 		$scope.$on('$locationChangeStart', function( event ) {
 			if($scope.edited) {
-				var answer = confirm("Are you sure you want to leave this page? All your modifications won't be saved.")
+				var answer = confirm("Are you sure you want to leave this page? All your modifications won't be saved.");
 				if (!answer) {
 					event.preventDefault();
 				}
@@ -125,6 +124,6 @@ This file provides the necessary controllers for the the recipe-detail.html page
 		});
 		
 		//ask confirmation before reloading the page
-		window.onbeforeunload = function confirmWinClose() { return "Are you sure you want to reload the page?";}
+		window.onbeforeunload = function confirmWinClose() { return "Are you sure you want to reload the page?";};
 	});
-})()
+})();
--- public/scripts/controllers/recipes.js
+++ public/scripts/controllers/recipes.js
@@ -13,46 +13,46 @@ This file provides the necessary controllers for the the recipe.html page to wor
 		//get a list of the recipes
 		$scope.getRecipes = function() {
 			dataService.getRecipes(function(response) {
-			  $scope.recipes = response.data
+			  $scope.recipes = response.data;
 			});
-		}
+		};
 		
 		//get a list of the categories
 		$scope.getCategories = function() {
 			dataService.getCategories(function(response) {
 			  $scope.categories = response.data;
 			});
-		}
+		};
 		
 		//refresh the lists 
 		$scope.clear = function() {
 			$scope.getRecipes();
 			$scope.getCategories();
-		}
+		};
 		
 		//filter all the recipe by a specific category
 		$scope.filterByCategory = function() {
 			dataService.getRecipesByCategory($scope.item.name, function(response) {
-			  $scope.recipes = response.data
+			  $scope.recipes = response.data;
 			});
-		}
+		};
 		 
 		//delete a specific recipe, with a confirmation box
 		$scope.deleteData = function(id) {
-			var answer = confirm("Are you sure you want to delete this recipe?")
+			var answer = confirm("Are you sure you want to delete this recipe?");
 			if (answer) {
 				dataService.delRecipeById(id);
 				$location.path('/').replace();
 			}
 			$scope.clear();
-		}
+		};
 		
 		//navigate to the recipe-detail.html, to add a new recipe
 		$scope.addNew = function() {
 			$location.path('/add').replace();
-		}
+		};
 		
 		//starting the app
 		$scope.clear();
 	});
-})()
+})();
